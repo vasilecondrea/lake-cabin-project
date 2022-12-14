5fb15babb83f57fa15aca344a1df630e7dc03a6e
Hana <h.mohiadin.13@gmail.com>
2022-12-14 12:28:33 +0000

    refactor with delete function and start dim_currency transformation and test

--- Data_Manipulation/src/transformation.py
+++ Data_Manipulation/src/transformation.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import io
+import json
 
 def read_files_from_s3_bucket(s3, bucket, key):
     
@@ -11,28 +12,46 @@ def read_files_from_s3_bucket(s3, bucket, key):
     addresses = ['New York', 'Paris']
     df['Address'] = addresses
 
+def delete_cols_from_df(df, col_list):
+    for col in col_list:
+        del df[col]
+    return df
+    
 def create_dim_counterparty(counterparty_df, address_df=None):
     dim_counterparty_df = counterparty_df.copy()
-    del dim_counterparty_df['commercial_contact']
-    del dim_counterparty_df['delivery_contact']
-    del dim_counterparty_df['created_at']
-    del dim_counterparty_df['last_updated']
+
+    cols_to_delete = ['commercial_contact', 'delivery_contact', 'created_at', 'last_updated']
+    dim_counterparty_df = delete_cols_from_df(dim_counterparty_df, cols_to_delete)
+
     address_df_copy = address_df.copy()
     address_df_copy = address_df_copy.rename(columns={'address_id':'legal_address_id'})
-    del address_df_copy['created_at']
-    del address_df_copy['last_updated']
+    cols_to_delete = ['created_at', 'last_updated']
+    address_df_copy = delete_cols_from_df(address_df_copy, cols_to_delete)
+
     result = pd.merge(dim_counterparty_df, address_df_copy, on='legal_address_id')
-    del result['legal_address_id']
+    result = delete_cols_from_df(result, ['legal_address_id']) 
+
     return result
 
 def create_dim_transaction(transaction_df):
     dim_transaction_df = transaction_df.copy()
-    del dim_transaction_df['created_at']
-    del dim_transaction_df['last_updated']
+
+    cols_to_delete = ['created_at', 'last_updated']
+    dim_transaction_df = delete_cols_from_df(dim_transaction_df, cols_to_delete)
+
     return dim_transaction_df
 
 def create_dim_payment_type(payment_df):
     dim_payment_type_df = payment_df.copy()
-    del dim_payment_type_df['created_at']
-    del dim_payment_type_df['last_updated']
+    
+    cols_to_delete = ['created_at', 'last_updated']
+    dim_payment_type_df = delete_cols_from_df(dim_payment_type_df, cols_to_delete)
+
     return dim_payment_type_df
+
+def create_dim_currency(currency_df):
+    
+    with open('currency-symbols.json') as f:
+        data = json.load(f)
+        for currency in currency_df['currency_code']:
+            pass

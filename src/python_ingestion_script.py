f4e320019e5601e9764f993fabdd60f708001c69
Shehryar Mughal <ShehryarMughal@Shehryars-MacBook-Pro.local>
2022-12-16 10:01:24 +0000

    Lambda function

--- src/python_ingestion_script.py
+++ src/python_ingestion_script.py
@@ -1,49 +1,49 @@
 import boto3
 import pg8000
 
-# Create an S3 client
-s3 = boto3.client('s3')
+def create_s3_buckets():
+    # Create an S3 client
+    s3 = boto3.client('s3')
 
-# Create the two S3 buckets
-s3.create_bucket(Bucket='ingested-data-bucket-1')
-s3.create_bucket(Bucket='processed-data-bucket-1')
+    # Create the two S3 buckets
+    s3.create_bucket(Bucket='ingested-data-bucket-1')
+    s3.create_bucket(Bucket='processed-data-bucket-1')
 
 
-# Connect to the PostgreSQL database
-conn = pg8000.connect(
-    host="nc-data-eng-totesys-production.chpsczt8h1nu.eu-west-2.rds.amazonaws.com",
-    port=5432,
-    user="project_user_4",
-    password="ZUr7UMAkA3mPQgrQ2jckFDfa",
-    database="totesys"
-)
-cursor = conn.cursor()
+def ingest_database_to_s3():
+    # Connect to the PostgreSQL database
+    conn = pg8000.connect(
+        host="nc-data-eng-totesys-production.chpsczt8h1nu.eu-west-2.rds.amazonaws.com",
+        port=5432,
+        user="project_user_4",
+        password="ZUr7UMAkA3mPQgrQ2jckFDfa",
+        database="totesys"
+        )
+    cursor = conn.cursor()
 
-# Retrieve all table names from the totesys database
-cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
-table_names = [row[0] for row in cursor.fetchall()]
+    # Retrieve all table names from the totesys database
+    cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+    table_names = [row[0] for row in cursor.fetchall()]
 
-print(table_names)
+    # Create a new S3 client
+    s3 = boto3.client("s3")
 
-# Create a new S3 client
-s3 = boto3.client("s3")
+    # Save the data from each table in a separate file in the "ingestion" S3 bucket
+    for table_name in table_names:
+        # Retrieve the data from the current table
+        cursor.execute(f"SELECT * FROM {table_name}")
+        rows = cursor.fetchall()
 
-# Save the data from each table in a separate file in the "ingestion" S3 bucket
-for table_name in table_names:
-    # Retrieve the data from the current table
-    cursor.execute(f"SELECT * FROM {table_name}")
-    rows = cursor.fetchall()
-    
-    # Save the data to a CSV file in the "ingestion" S3 bucket
-    with open(f"{table_name}.csv", "w") as file:
-        for row in rows:
-            file.write(",".join([str(cell) for cell in row]))
-            file.write("\n")
-    s3.upload_file(f"{table_name}.csv", "ingested-data-bucket-1", f"{table_name}.csv")
+        # Save the data to a CSV file in the "ingestion" S3 bucket
+        with open(f"{table_name}.csv", "w") as file:
+            for row in rows:
+                file.write(",".join([str(cell) for cell in row]))
+                file.write("\n")
+        s3.upload_file(f"{table_name}.csv", "ingested-data-bucket-1", f"{table_name}.csv")
 
-# Close the database connection
-cursor.close()
-conn.close()
+    # Close the database connection
+    cursor.close()
+    conn.close()
 
 
 
--- test/test_python_ingestion_script.py
+++ test/test_python_ingestion_script.py
@@ -1,15 +1,53 @@
-import moto
-import boto3
+from moto import mock_s3
+from unittest import TestCase
+from src.python_ingestion_script import create_s3_buckets
+from src.python_ingestion_script import ingest_database_to_s3
 
-# Test that the two S3 buckets are created correctly
-@moto.mock_s3
-def test_create_buckets():
-    s3 = boto3.client('s3')
-    s3.create_bucket(Bucket='ingested-data-bucket-1')
-    s3.create_bucket(Bucket='processed-data-bucket-1')
 
-    # Verify that the buckets exist
-    response = s3.list_buckets()
-    bucket_names = [bucket['Name'] for bucket in response['Buckets']]
-    assert 'ingested-data-bucket-1' in bucket_names
-    assert 'processed-data-bucket-1' in bucket_names
+@mock_s3
+class TestCreateS3BucketsAndIngestDatabaseToS3(TestCase):
+    def test_create_s3_buckets(self):
+        create_s3_buckets()
+
+        # Verify that the two S3 buckets were created
+        s3 = boto3.client("s3")
+        buckets = s3.list_buckets()["Buckets"]
+        bucket_names = [bucket["Name"] for bucket in buckets]
+        self.assertIn("ingested-data-bucket-1", bucket_names)
+        self.assertIn("processed-data-bucket-1", bucket_names)
+
+    def test_ingest_database_to_s3(self):
+        # Connect to the mock PostgreSQL database
+        conn = pg8000.connect(
+            host="nc-data-eng-totesys-production.chpsczt8h1nu.eu-west-2.rds.amazonaws.com",
+            port=5432,
+            user="project_user_4",
+            password="ZUr7UMAkA3mPQgrQ2jckFDfa",
+            database="totesys"
+            )
+        cursor = conn.cursor()
+
+        # Create some mock tables in the database
+        cursor.execute("CREATE TABLE table1 (col1 INT, col2 INT)")
+        cursor.execute("CREATE TABLE table2 (col1 INT, col2 INT)")
+        cursor.execute("CREATE TABLE table3 (col1 INT, col2 INT)")
+
+        # Add some mock data to the tables
+        cursor.execute("INSERT INTO table1 VALUES (1, 2)")
+        cursor.execute("INSERT INTO table2 VALUES (3, 4)")
+        cursor.execute("INSERT INTO table3 VALUES (5, 6)")
+
+        # Close the database connection
+        cursor.close()
+        conn.close()
+
+        # Ingest the mock database to the S3 bucket
+        ingest_database_to_s3()
+
+        # Verify that the CSV files were created and uploaded to the "ingested-data-bucket-1" bucket
+        s3 = boto3.client("s3")
+        bucket = s3.list_objects(Bucket="ingested-data-bucket-1")
+        self.assertEqual(bucket["KeyCount"], 3)
+        self.assertIn("table1.csv", bucket["Contents"])
+        self.assertIn("table2.csv", bucket["Contents"])
+        self.assertIn("table3.csv", bucket["Contents"])

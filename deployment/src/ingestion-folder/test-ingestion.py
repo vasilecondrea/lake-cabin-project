67575341580080cb39683a5a7362bb1cb57432d8
Sam Fowler <samuelfowler291297@gmail.com>
2022-12-19 09:17:55 +0000

    Moved bucket creation into deployment script and removed duplicate ingestion-bucket variable that was causing issues

--- deployment/src/ingestion-folder/test-ingestion.py
+++ deployment/src/ingestion-folder/test-ingestion.py
@@ -5,5 +5,5 @@ logger = logging.getLogger("lambdaLogger")
 logger.setLevel = logging.info
 
 
-def ingestion_handler(event, context):
-    logger.info("testing the ingestion function for deployment")
+def lambda_handler(event, context):
+    print(event)

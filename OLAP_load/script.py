1b0c736d6bcab2333e31e0184c49a51410dd7f78
vasilecondrea <vasile.condrea@gmail.com>
2022-12-20 11:54:29 +0000

    written test and code to received the data from the processed bucket

--- OLAP_load/script.py
+++ OLAP_load/script.py
@@ -1,16 +1,46 @@
 
+import boto3
+import botocore
+
+
+# s3 = boto3.client("s3")
+
 
 def lambda_handler(event, context):
     pass
 
 
-def list_tables(processed_bucket):
-    pass
+def list_tables(s3, processed_bucket):
+
+    if not isinstance(s3, botocore.client.BaseClient):
+        raise (ValueError)
+
+    if type(processed_bucket) != str:
+        raise (ValueError)
+
+    response = s3.list_objects_v2(Bucket=processed_bucket)
+
+    tables = [table["Key"] for table in response["Contents"]]
+    return tables
 
 
-def load_parquet_file(object_name):
-    pass
+def load_table_from_name(s3, processed_bucket, table_name):
 
+    if not isinstance(s3, botocore.client.BaseClient):
+        raise (ValueError)
+
+    if type(processed_bucket) != str:
+        raise (ValueError)
+
+    if type(table_name) != str:
+        raise (ValueError)
+
+    response = s3.get_object(Bucket=processed_bucket, Key=table_name)
+
+    print(response)
+
+
+# def
 
 def upload_to_OLAP():
     pass
--- OLAP_load/test/test.py
+++ OLAP_load/test/test.py
@@ -2,14 +2,50 @@ from moto import mock_s3
 import moto
 import boto3
 from datetime import datetime, timedelta
+from unittest.mock import MagicMock
+import pytest
 
-from OLAP_load.script import lambda_handler, list_tables, load_parquet_file, upload_to_OLAP
 
-# from src.script import list_updated_tables
+from OLAP_load.script import lambda_handler, list_tables, load_table_from_name, upload_to_OLAP
 
 
 def test_lambda_handler():
     pass
+
+
+@mock_s3
+def test_list_table_calls_the_object_v2_with_bucket_name():
+
+    s3 = boto3.client("s3")
+    bucket = "test_bucket"
+    s3.list_objects_v2 = MagicMock()
+
+    list_tables(s3, bucket)
+    s3.list_objects_v2.assert_called_with(Bucket=bucket)
+
+
+@mock_s3
+def test_list_tables_returns_error_when_passed_invalid_args():
+    s3 = boto3.client("s3")
+
+    with pytest.raises(ValueError):
+        list_tables(3, "valid_bucket_name")
+
+    with pytest.raises(ValueError):
+        list_tables(s3, 4)
+
+
+# @mock_s3
+# def test_list_tables_catches_boto3_errors():
+
+#     valid_bucket = "valid_bucket"
+#     invalid_bucket = "invalid_bucket"
+#     s3 = boto3.client("s3")
+
+#     s3.create_bucket(Bucket=valid_bucket)
+
+#     with pytest.raises(ValueError):
+#         list_tables(s3, invalid_bucket)
 
 
 @mock_s3
@@ -23,7 +59,7 @@ def test_list_tables():
     s3.create_bucket(Bucket=bucket)
     s3.upload_file(Filename=file, Bucket=bucket, Key=key)
 
-    result=0# = list_updated_tables(bucket)
+    result = list_tables(s3, bucket)
 
     assert result == ['test_parquet']
 
@@ -42,9 +78,33 @@ def test_list_tables():
 #     old_time_five_min = datetime.now() - timedelta(minutes=5)
 
 #     moto.s3.models.s3_backends['123456789012']['global'].buckets[bucket].keys[key].last_modified = old_time_five_min
-    
+
 #     print(moto.s3.models.s3_backends['123456789012']['global'].buckets[bucket].keys[key].last_modified)
 
 #     print(s3.list_objects(Bucket=bucket)['Contents'])
 
 #     assert False
+
+@mock_s3
+def test_load_table_from_name_raises_ValueError_when_passed_invalid_args():
+    with pytest.raises(ValueError):
+        load_table_from_name(boto3.client("s3"), 4, 'table_name')
+
+    with pytest.raises(ValueError):
+        load_table_from_name(boto3.client("s3"), 'bucket_name', 3)
+
+    with pytest.raises(ValueError):
+        load_table_from_name(4, 'bucket_name', 'table_name')
+
+
+@mock_s3
+def test_load_from_name_calls_get_objects_with_correct_args():
+
+    s3 = boto3.client("s3")
+    bucket = "test_bucket"
+
+    key = "test_table"
+    s3.get_object = MagicMock()
+
+    load_table_from_name(s3, bucket, key)
+    s3.get_object.assert_called_with(Bucket=bucket, Key=key)

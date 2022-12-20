5e12c049755d0607687f6644347023ef8ded1ee6
Shehryar Mughal <ShehryarMughal@Shehryars-MacBook-Pro.local>
2022-12-20 14:12:38 +0300

    Add AWS Secrets Managaer

--- src/python_ingestion_script.py
+++ src/python_ingestion_script.py
@@ -13,18 +13,30 @@ def create_s3_buckets():
     s3.create_bucket(Bucket='ingested-data-bucket-1')
     s3.create_bucket(Bucket='processed-data-bucket-1')
 
-def ingest_database_to_s3():
-    try:
-        # Connect to the PostgreSQL database
-        conn = pg8000.connect(
-            host="nc-data-eng-totesys-production.chpsczt8h1nu.eu-west-2.rds.amazonaws.com",
-            port=5432,
-            user="project_user_4",
-            password="ZUr7UMAkA3mPQgrQ2jckFDfa",
-            database="totesys"
-            )
-        cursor = conn.cursor()
 
+def ingest_database_to_s3():
+    # Retrieve the database connection details from AWS Secrets Manager
+    secrets_manager_client = boto3.client("secretsmanager")
+    secret_value_response = secrets_manager_client.get_secret_value(SecretId="Source Database Creds") #Change this to match
+    secret_dict = json.loads(secret_value_response["SecretString"])
+
+    host = secret_dict["host"]
+    port = secret_dict["port"]
+    user = secret_dict["username"]
+    password = secret_dict["password"]
+    database = secret_dict["database"]
+
+    # Connect to the PostgreSQL database
+    conn = pg8000.connect(
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        database=database
+    )
+    cursor = conn.cursor()
+
+    try:
         # Retrieve all table names from the totesys database
         cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
         table_names = [row[0] for row in cursor.fetchall()]
@@ -35,7 +47,7 @@ def ingest_database_to_s3():
             cursor.execute(f"SELECT max(last_update) FROM {table_name}")
             last_update = cursor.fetchone()[0]
             # If the table has been modified, retrieve and save the updated data
-            if last_update > datetime.datetime.utcnow() - datetime.timedelta(hours=0.5):
+            if last_update > datetime.datetime.utcnow() - datetime.timedelta(hours=0.5): #Change this to what you decide on
                 # Retrieve the data from the current table
                 cursor.execute(f"SELECT * FROM {table_name}")
                 rows = cursor.fetchall()
@@ -71,60 +83,6 @@ def ingest_database_to_s3():
             }],
             Namespace='Ingestion'
         )
-        cloudwatch.put_log_events(
-            LogGroupName='Ingestion',
-            LogStreamName='Error',
-            Events=[{
-                'Timestamp': datetime.datetime.utcnow(),
-                'Message': str(e)
-            }]
-        )
-
-
-# def ingest_database_to_s3():
-#     try:
-#         # Connect to the PostgreSQL database
-#         conn = pg8000.connect(
-#             host="nc-data-eng-totesys-production.chpsczt8h1nu.eu-west-2.rds.amazonaws.com",
-#             port=5432,
-#             user="project_user_4",
-#             password="ZUr7UMAkA3mPQgrQ2jckFDfa",
-#             database="totesys"
-#             )
-#         cursor = conn.cursor()
-
-#         # Retrieve all table names from the totesys database
-#         cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
-#         table_names = [row[0] for row in cursor.fetchall()]
-
-#         # Save the data from each table in a separate file in the "ingestion" S3 bucket
-#         for table_name in table_names:
-#             # Retrieve the data from the current table
-#             cursor.execute(f"SELECT * FROM {table_name}")
-#             rows = cursor.fetchall()
-
-#             # Save the data to a CSV file in the "ingestion" S3 bucket
-#             with open(f"{table_name}.csv", "w") as file:
-#                 for row in rows:
-#                     file.write(",".join([str(cell) for cell in row]))
-#                     file.write("\n")
-#             s3.upload_file(f"{table_name}.csv", "ingested-data-bucket-1", f"{table_name}.csv")
-
-        # Close the database connection
-        cursor.close()
-        conn.close()
-
-    except Exception as e:
-        # Log the error message to CloudWatch
-        cloudwatch.put_metric_data(
-            MetricData=[{
-                'MetricName': 'IngestionError',
-                'Timestamp': datetime.datetime.utcnow(),
-                'Value': 1
-            }],
-            Namespace='Ingestion'
-        )
-        cloudwatch.put_log_
 
 
 def lambda_handler(event, context):

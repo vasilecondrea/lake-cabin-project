59d8d479ea28c0c00c1390fe390bd5cfc90ef33d
Sam Fowler <samuelfowler291297@gmail.com>
2022-12-21 13:44:09 +0000

    Removes creation of s3 buckets and cloudwatch putmetricdata

--- src/python_ingestion_script.py
+++ src/python_ingestion_script.py
@@ -1,6 +1,7 @@
 import boto3
 import pg8000
 import datetime
+import json
 
 # Create an S3 client
 s3 = boto3.client('s3')
@@ -8,16 +9,10 @@ s3 = boto3.client('s3')
 # Create a CloudWatch client
 cloudwatch = boto3.client('cloudwatch')
 
-def create_s3_buckets():
-    # Create the two S3 buckets
-    s3.create_bucket(Bucket='ingested-data-bucket-1')
-    s3.create_bucket(Bucket='processed-data-bucket-1')
-
-
-def ingest_database_to_s3():
+def ingest_database_to_s3(bucket_name):
     # Retrieve the database connection details from AWS Secrets Manager
-    secrets_manager_client = boto3.client("secretsmanager")
-    secret_value_response = secrets_manager_client.get_secret_value(SecretId="db-creds-source") #Change this to match
+    secretsmanager = boto3.client("secretsmanager")
+    secret_value_response = secretsmanager.get_secret_value(SecretId="db-creds-source") #Change this to match
     secret_dict = json.loads(secret_value_response["SecretString"])
 
     host = secret_dict["host"]
@@ -57,61 +52,29 @@ def ingest_database_to_s3():
                     for row in rows:
                         file.write(",".join([str(cell) for cell in row]))
                         file.write("\n")
-                s3.upload_file(f"{table_name}.csv", "ingested-data-bucket-1", f"{table_name}.csv")
+                s3.upload_file(f"tmp/{table_name}.csv", bucket_name, f"{table_name}.csv")
             else:
                 # Log a message to CloudWatch indicating that the table has not been modified
-                cloudwatch.put_log_events(
-                    LogGroupName='Ingestion',
-                    LogStreamName=table_name,
-                    Events=[{
-                        'Timestamp': datetime.datetime.utcnow(),
-                        'Message': 'Table has not been modified'
-                    }]
-                )
+                print(f'[INGESTION] {table_name} has not been modified')
 
         # Close the database connection
         cursor.close()
         conn.close()
 
     except Exception as e:
-        # Log the error message to CloudWatch
-        cloudwatch.put_metric_data(
-            MetricData=[{
-                'MetricName': 'IngestionError',
-                'Timestamp': datetime.datetime.utcnow(),
-                'Value': 1
-            }],
-            Namespace='Ingestion'
-        )
+        # Log the error message to CloudWatch        
+        print(f'[INGESTION] ERROR: {e}')
 
 
 def lambda_handler(event, context):
     # Log the start time of the function execution
-    cloudwatch.put_metric_data(
-        MetricData=[{
-            'MetricName': 'IngestionStartTime',
-            'Timestamp': datetime.datetime.utcnow(),
-            'Value': 1
-        }],
-        Namespace='Ingestion'
-    )
-
-    # Create the S3 buckets
-    create_s3_buckets()
+    print(f'[INGESTION] Ingestion started')
 
     # Ingest the database to S3
-    ingest_database_to_s3()
+    ingest_database_to_s3(event['ingested_bucket'])
 
     # Log the end time of the function execution
-    cloudwatch.put_metric_data(
-        MetricData=[{
-            'MetricName': 'IngestionEndTime',
-            'Timestamp': datetime.datetime.utcnow(),
-            'Value': 1
-        }],
-        Namespace='Ingestion'
-    )
-
+    print(f'[INGESTION] Ingestion completed')
 
 
 

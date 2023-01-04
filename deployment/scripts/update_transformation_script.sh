e51180021bdc5f1c4246e9baaffa3092ef5d2014
Sam Fowler <samuelfowler291297@gmail.com>
2023-01-04 16:44:43 +0000

    Fixes deployment script to work with new file structure and naming scheme

--- deployment/scripts/update_transformation_script.sh
+++ deployment/scripts/update_transformation_script.sh
@@ -1,16 +1,18 @@
 echo "Creating transformation function deployment package..."
-cd ../transformation/src/data_transformation_code/package 
-zip --q -r ../transformation.zip .
+cd ../transform/src/package 
+zip --q -r ../transform.zip .
 cd ../
-zip --q transformation.zip transformation_helper.py transformation_lambda.py transformation_retrieve.py transformation_tables.py transformation_upload.py currency-symbols.json
-cd ../../../deployment
+zip --q transform.zip transform_helper.py transform.py transform_retrieve.py transform_tables.py transform_upload.py currency-symbols.json
+cd ../../deployment
 wait
 
 echo "Uploading transformation deployment package..."
-aws s3 cp ../transformation/src/data_transformation_code/transformation.zip s3://${CODE_BUCKET_NAME}/${TRANSFORMATION_FUNCTION_NAME}/transformation.zip >> $LOG_PATH/deployment_log_${SUFFIX}.out
+aws s3 cp ../transform/src/transform.zip s3://${CODE_BUCKET_NAME}/${TRANSFORMATION_FUNCTION_NAME}/transform.zip >> $LOG_PATH/deployment_log_${SUFFIX}.out
 wait
+
+rm ../transform/src/transform.zip
 
 aws lambda update-function-code --function-name ${TRANSFORMATION_FUNCTION_NAME} \
 --s3-bucket ${CODE_BUCKET_NAME} \
---s3-key ${TRANSFORMATION_FUNCTION_NAME}/transformation.zip
+--s3-key ${TRANSFORMATION_FUNCTION_NAME}/transform.zip
 wait

64e82039474f247368136d06c5819f3533d8f67f
AnthonyWongGit <anthwong@hotmail.co.uk>
2023-01-04 08:54:48 +0000

    Revert "renamed and deleted some directories"
    This reverts commit e4d391e77ebaa96cf6e9333fd36ce5a9d15177a5.

--- deployment/scripts/update_transformation_script.sh
+++ deployment/scripts/update_transformation_script.sh
@@ -1,5 +1,5 @@
 echo "Creating transformation function deployment package..."
-cd ../transformation/src/data_transformation_code/package 
+cd ../Data_Manipulation/src/data_transformation_code/package 
 zip --q -r ../transformation.zip .
 cd ../
 zip --q transformation.zip transformation_helper.py transformation_lambda.py transformation_retrieve.py transformation_tables.py transformation_upload.py currency-symbols.json
@@ -7,7 +7,7 @@ cd ../../../deployment
 wait
 
 echo "Uploading transformation deployment package..."
-aws s3 cp ../transformation/src/data_transformation_code/transformation.zip s3://${CODE_BUCKET_NAME}/${TRANSFORMATION_FUNCTION_NAME}/transformation.zip >> $LOG_PATH/deployment_log_${SUFFIX}.out
+aws s3 cp ../Data_Manipulation/src/data_transformation_code/transformation.zip s3://${CODE_BUCKET_NAME}/${TRANSFORMATION_FUNCTION_NAME}/transformation.zip >> $LOG_PATH/deployment_log_${SUFFIX}.out
 wait
 
 aws lambda update-function-code --function-name ${TRANSFORMATION_FUNCTION_NAME} \
